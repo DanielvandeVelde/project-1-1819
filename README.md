a3382bd4d6918a5b8ecb50cf3c036f49c08b70e4
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2019-03-05 11:23:49 +0100

    changes

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Project 1 @cmda-minor-web · 2018-2019
 
-## Zoek in de de collectie van de OBA
+## Idee
 
-Het project vindt plaats bij de Centrale OBA. Maandagochtend is om 10.00 uur de kickoff, vrijdag zijn de presentaties van de resultaten. In een week bouwt iedere student een eigen prototype dat bezoekers kan helpen om items uit de OBA collectie te vinden. Technieken geleerd bij [CSS to the Rescue](https://github.com/cmda-minor-web/css-to-the-rescue-1819) en [Web App from Scratch](https://github.com/cmda-minor-web/web-app-from-scratch-1819) worden toegepast bij het bouwen van de de prototypes.
+De jongere bezoekers van de OBA die pas net begonnen zijn met lezen te stimuleren door een website te laten zien waarop zij een troffee zien met hun naam en de titel van het boek wat zij zojuist hebben uitgelezen.
 
-We verzamelen maandagochtend om 09.45 op de 1e verdieping van de Centrale OBA, [Oosterdokskade 143](https://www.google.com/maps/place/OBA+library+of+Amsterdam/@52.3756983,4.9082087,15z/data=!4m2!3m1!1s0x0:0x6b97f693e6ecb494?ved=2ahUKEwioo6OrzubgAhWLNOwKHR6KDQgQ_BIwCnoECAYQCA).
+Deze is te printen via de 3D printer aanwezig in de OBA.
 
-
+[OBA-Wrapper](https://github.com/maanlamp/OBA-wrapper)
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,111 @@
 
+  function getScene() {
+    var scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x111111);
+    return scene;
+  }
+
+  function getCamera() {
+    var aspectRatio = window.innerWidth / window.innerHeight;
+    var camera = new THREE.PerspectiveCamera(25, aspectRatio, 0.1, 1000);
+    camera.position.set(0, 1, 5);
+    return camera;
+  }
+
+  function getLight(scene) {
+    var light = new THREE.PointLight(0xffffff, 1, 0);
+    light.position.set(1, 1, 1);
+    scene.add(light);
+
+    var ambientLight = new THREE.AmbientLight(0x111111);
+    scene.add(ambientLight);
+    return light;
+  }
+
+  function getRenderer() {
+    // Create the canvas with a renderer
+    var renderer = new THREE.WebGLRenderer({antialias: false});
+    // Add support for retina displays
+    renderer.setPixelRatio(window.devicePixelRatio);
+    // Specify the size of the canvas
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // Add the canvas to the DOM
+    document.body.appendChild(renderer.domElement);
+    return renderer;
+  }
+
+  function getControls(camera, renderer) {
+    var controls = new THREE.TrackballControls(camera, renderer.domElement);
+    controls.zoomSpeed = 0.4;
+    controls.panSpeed = 0.4;
+    return controls;
+  }
+
+  var PIXEL_RATIO = (function () {
+    var ctx = document.createElement('canvas').getContext('2d'),
+        dpr = window.devicePixelRatio || 1,
+        bsr = ctx.webkitBackingStorePixelRatio ||
+              ctx.mozBackingStorePixelRatio ||
+              ctx.msBackingStorePixelRatio ||
+              ctx.oBackingStorePixelRatio ||
+              ctx.backingStorePixelRatio || 1;
+    return dpr / bsr;
+  })();
+
+
+  createRetinaCanvas = function(w, h, ratio) {
+    if (!ratio) { ratio = PIXEL_RATIO; }
+    var can = document.createElement('canvas');
+    can.width = w * ratio;
+    can.height = h * ratio;
+    can.style.width = w + 'px';
+    can.style.height = h + 'px';
+    can.getContext('2d').setTransform(ratio, 0, 0, ratio, 0, 0);
+    return can;
+  }
+
+  function addTexture(name = 'Daniel') {
+    var text = name;
+    //create image
+    var bitmap = createRetinaCanvas(256, 256);
+    var ctx = bitmap.getContext('2d', {antialias: false});
+    ctx.font = 'Bold 20px Arial';
+
+    ctx.beginPath();
+    ctx.rect(0, 0, 256, 256);
+    ctx.fillStyle = 'gold';
+    ctx.fill();
+
+    ctx.fillStyle = 'white';
+    var textWidth = ctx.measureText(text).width;
+    ctx.fillText(text , 128 - (textWidth / 2), 128);
+    ctx.strokeStyle = 'black';
+    ctx.strokeText(text, 128 -(textWidth / 2), 128);
+    ctx.textAlign = "center";
+
+    // canvas contents will be used for a texture
+    var texture = new THREE.Texture(bitmap)
+    texture.needsUpdate = true;
+    var material = new THREE.MeshBasicMaterial({ map: texture });
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube)
+  }
+
+  // Render loop
+  function render() {
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+    controls.update();
+  };
+
+  var scene = getScene();
+  var camera = getCamera();
+  var light = getLight(scene);
+  var renderer = getRenderer();
+  var controls = getControls(camera, renderer);
+  var cube;
+
+  addTexture()
+
+  render();
